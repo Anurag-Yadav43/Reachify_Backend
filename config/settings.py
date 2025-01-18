c9aa95969b3d287483d163ce26f45bdb8a7a4145
Anurag Yadav <anuragvns2018@gmail.com>
2025-01-19 00:09:32 +0530

    Allowed Hosts

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f_nr*5-g@c7@ekp@=rs=f@0#vyv$&*!@ipqrp=(=1%@4o@1zo5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reachifybackend.azurewebsites.net']
 
 
 # Application definition
